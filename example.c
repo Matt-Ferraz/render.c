b1f835d61f86ab38d89945caa6661735e189398d
Mateus Henrique Ferraz <ferraz_dev@proton.me>
2024-05-21 14:07:39 -0300

    add fill method with ppm images

--- example.c
+++ example.c
@@ -1,8 +1,22 @@
 #include "stdio.h"
 #include "render.c"
+#include <errno.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#define WIDTH 800
+#define HEIGHT 600
+
+// allocating memory
+static uint32_t pixels[HEIGHT*WIDTH];
 
 int main(void) {
-    printf("Hello world");
-
+    renderc_fill(pixels, WIDTH, HEIGHT, 0xFF00FF00);
+    const char *file_path = "out.ppm";
+    Errno err = renderc_save_to_ppm(pixels, WIDTH, HEIGHT, file_path);
+    if(err) {
+        fprintf(stderr, "FAIL: could not complete file");
+        return 1;
+    }
     return 0;
 }
